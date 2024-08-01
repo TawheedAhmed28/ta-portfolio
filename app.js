46e861a12889d29430b43098cb1c109ec6367ce8
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-08-01 12:24:11 +0100

    reformatting of project description and SVGs

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const body = document.querySelector("body")
 const linkedin = document.getElementById("linkedin-svg")
 const email = document.getElementById("email-svg")
 
+const aboutMeDescription = document.getElementById("about-me-description")
+const showMoreDescription = document.getElementById("show-about-me")
+
 const prevButton = document.getElementById("previous-project")
 const projectNumberText = document.getElementById("project-number")
 const nextButton = document.getElementById("next-project")
@@ -20,10 +23,10 @@ const prevImage = document.getElementById("prev-image")
 const nextImage = document.getElementById("next-image")
 
 
-window.onbeforeunload = () => {stylesheet.href = "index.css"}
+window.onbeforeunload = () => { stylesheet.href = "index.css" }
 
 window.onload = async () => {
-    
+
     prevButton.classList.remove("duration-400")
     nextButton.classList.remove("duration-400")
     prevButton.classList.add("delay-[2000ms]", "duration-1000")
@@ -44,9 +47,12 @@ window.onload = async () => {
     linkedin.classList.add("delay-[900ms]", "duration-1000")
     email.classList.add("delay-[900ms]", "duration-1000")
 
+    showMoreDescription.classList.remove("duration-400")
+    showMoreDescription.classList.add("delay-1000", "duration-1000")
+
     await sleep(2300)
     refactorCSS()
-    
+
 }
 
 
@@ -58,51 +64,51 @@ projectNumberText.innerText = `Project ${projectNumber} of 4`
 projectImage.src = `images/${projectNames[projectNumber - 1]}/${projectNames[projectNumber - 1]}${imagePage}.png`
 
 const descriptions = [
-    
+
     "Wordle - solo project, 1 week\nTechnologies: HTML, CSS, JavaScript\n\nI built a game from a given list using the technologies above. I decided on Wordle as I was already very familiar with the game. The functionality of checking the win criteria proved very challenging, so it allowed me to greatly expand my knowledge of JavaScript and HTML.",
-    
-    "Capsaicin Station - solo project, 1 week\nTechnologies: EJS, CSS, JavaScript, MongoDB\n\nFull stack MEN app with CRUD functionality - I created an app where users can post and see information about chillies. I used two models, a user model and a chilli model, which was embedded within the user model to allow for each chilli to have “owners”.", 
-    
+
+    "Capsaicin Station - solo project, 1 week\nTechnologies: EJS, CSS, JavaScript, MongoDB\n\nFull stack MEN app with CRUD functionality - I created an app where users can post and see information about chillies. I used two models, a user model and a chilli model, which was embedded within the user model to allow for each chilli to have “owners”.",
+
     "Joystickipedia - paired project, 1 week\nTechnologies: JSX, JavaScript, React, CSS (Tailwind), React Select\n\nFull stack MERN app with CRUD functionality. Users can post games, and add them to a drop down list of genres. On each game, users can also add reviews. For this project, I focused more on the front end, and especially on review functionality. I wanted to allow users to provide a star rating, so using React.js to allow star emojis to represent rating values, and Tailwind to alter the opacity of the stars, I was able to create an easy to understand user interface for doing so.",
-    
+
     "Pizza Delivery - solo project, 1 week\nTechnologies: Python, Django, React, CSS (Tailwind), React Select, JSX, Postgres\n\nFull stack Python-Django-SQL app with CRUD functionality. Users can post pizzas and add toppings to them from a drop down menu. Users can also post pizza orders, which also have CRUD functionality."
-    
+
 ]
 
 const readmes = [
-    
+
     "https://github.com/TawheedAhmed28/wordle-game-project",
-    
+
     "https://github.com/TawheedAhmed28/chilli-database-project",
-    
+
     "https://github.com/KizzaThorley/gamereview-project-frontend",
-    
+
     "https://github.com/TawheedAhmed28/pizza-delivery-project-frontend"
-    
+
 ]
 
 const deploys = [
-    
+
     "https://tawheedahmed28.github.io/wordle-game-project/",
-    
+
     "https://capsaicin-station.netlify.app/",
-    
+
     "https://joystickipedia.netlify.app/",
-    
+
     "https://pizza-delivery-api.netlify.app/"
-    
+
 ]
 
 const projectImages = [
-    
+
     ["Wordle1", "Wordle2", "Wordle3"],
-    
+
     ["Chilli1", "Chilli2"],
-    
+
     ["Games1", "Games2", "Games3"],
-    
+
     ["Pizza1", "Pizza2", "Pizza3", "Pizza4", "Pizza5"]
-    
+
 ]
 
 imageNumber.innerText = `Image ${imagePage} of ${projectImages[projectNumber - 1].length}`
@@ -138,6 +144,9 @@ function refactorCSS() {
     linkedin.classList.add("duration-400")
     email.classList.add("duration-400")
 
+    showMoreDescription.classList.remove("delay-1000", "duration-1000")
+    showMoreDescription.classList.add("duration-400")
+
 }
 
 function changePage() {
@@ -156,6 +165,12 @@ function changeImage() {
 
     projectImage.src = `images/${projectNames[projectNumber - 1]}/${projectNames[projectNumber - 1]}${imagePage}.png`
     imageNumber.innerText = `Image ${imagePage} of ${projectImages[projectNumber - 1].length}`
+
+}
+
+function changeDescription() {
+
+    const lessDesc = `I first got an interest in coding since I first started gaming, I guess - I just didn't realise it at the time but I'd always been exploring with and messing with different aspects of games, like for example what each item in a game does.Now I understand games better than before, so when I looked at game mods or game files, I could make some sense of them, even before starting the Software Engineering Bootcamp at General Assembly. I had no knowledge of coding, but I sure had knowledge of games, and I could use that knowledge to know what the code does.Once I realised that this - the concept of trial and error and always messing with things until they do what you want them to - was essentially coding, I was determined to learn how to code and find a career in it... <span class="cursor-pointer transition opacity-100 translate-y-2 duration-1000 delay-1000 font-thin text-purple-400 text-[20px] underline hover:text-green-500" id="show-about-me">show more</span>`
 
 }
 
